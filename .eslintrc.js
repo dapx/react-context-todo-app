c70ac6cdf785b55e676cbdf4d4c68a4ba58b3878
Daniel André da Silva <dap1995@gmail.com>
2018-05-30 13:04:31 -0300

    Add airbnb eslint config and others default plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
   "extends" : [
     "react-app",
-  ]
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb-base"
+  ],
+  "plugins": [
+    "react",
+    // Warns about potential accessibility issues with your React elements.
+    "jsx-a11y",
+  ],
+  "rules": {
+    // Allow .js files with jsx
+    "react/jsx-filename-extension": "off",
+    // Disables the need to use `this` on class methods.
+    "class-methods-use-this": "off",
+  },
 };
