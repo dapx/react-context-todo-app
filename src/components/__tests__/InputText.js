449118c04cde004afdf4091235068cdbe95cf504
Daniel André da Silva <dap1995@gmail.com>
2018-05-31 20:13:05 -0300

    Test InputText state changes

--- src/components/__tests__/InputText.js
+++ src/components/__tests__/InputText.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import InputText from '../InputText';
 
 it('renders without crashing', () => {
   const input = document.createElement('input');
   ReactDOM.render(<InputText />, input);
+});
+
+it('Should render input text empty when component is mounted', () => {
+  const wrapper = shallow(<InputText onSubmit={() => {}} />);
+  expect(wrapper).toHaveState('text');
+  expect(wrapper.state('text')).toEqual('');
+});
+
+it('Should submit text when pressed the enter key', () => {
+  const handleSubmit = jest.fn();
+  const wrapper = mount(<InputText onSubmit={handleSubmit} />);
+  const input = wrapper.find('input');
+
+  input.simulate('keydown', { key: 'Enter' });
+  expect(handleSubmit).toBeCalledWith('');
+
+  input.simulate('change', { target: { value: 'some value' } });
+  input.simulate('keydown', { key: 'Enter' });
+  expect(handleSubmit).toBeCalledWith('some value');
+});
+
+it('Should clear text input when pressed enter key', () => {
+  const handleSubmit = jest.fn();
+  const wrapper = mount(<InputText onSubmit={handleSubmit} />);
+  const input = wrapper.find('input');
+
+  input.simulate('change', { target: { value: 'some value' } });
+  expect(wrapper.state('text')).toEqual('some value');
+
+  input.simulate('keydown', { key: 'Enter' });
+  expect(handleSubmit).toBeCalledWith('some value');
+
+  expect(wrapper.state('text')).toEqual('');
+  input.simulate('keydown', { key: 'Enter' });
+  expect(handleSubmit).toBeCalledWith('');
 });
