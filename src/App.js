af155679e0965056d8fb94d3ef3df0afaecdfc05
Daniel André da Silva <dap1995@gmail.com>
2018-05-31 18:32:45 -0300

    Create App layout with styled components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
+// @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import InputText from './components/InputText';
 
-class App extends Component {
+const Window = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  margin: 0 10% 0 10%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  margin: 0 0 -40px 0;
+`;
+
+const Paper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const Title = styled.h1`
+  display: flex;
+  font-size: 100px;
+  font-weight: 100;
+  justify-content: center;
+  color: rgba(175, 47, 47, 0.15);
+  text-rendering: optimizeLegibility;
+`;
+
+type Props = {};
+
+class App extends Component<Props> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Window>
+        <Container>
+          <Header>
+            <Title>todos</Title>
+          </Header>
+          <Paper>
+            <InputText onSubmit={() => {}} />
+          </Paper>
+        </Container>
+      </Window>
     );
   }
 }
