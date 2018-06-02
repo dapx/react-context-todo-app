d094776b5938b3f21337425c3d0479f52eb4e0fb
Daniel André da Silva <dap1995@gmail.com>
2018-06-01 22:41:49 -0300

    Test render button

---
+++ src/components/__tests__/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '../Button';
+
+it('renders without crashing', () => {
+  const button = document.createElement('button');
+  ReactDOM.render(<Button onClick={() => {}} />, button);
+});
