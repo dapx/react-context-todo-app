4b514088a8027634bc30a84d21dee63b1ca2b991
Daniel André da Silva <dap1995@gmail.com>
2018-06-02 12:02:35 -0300

    Create List module to get information from the Todo list

---
+++ src/modules/List.js
@@ -0,0 +1,27 @@
+/**
+ * This module provides useful pure functions
+ * to obtain information from a todo List.
+ */
+
+export const VisibilityFilter = {
+  NONE: 'NONE',
+  DONE: 'DONE',
+  TODO: 'TODO'
+};
+
+export function filterListByVisibility(list = [], visibility) {
+  switch (visibility) {
+    case VisibilityFilter.DONE:
+      return list.filter(todo => todo.isDone);
+
+    case VisibilityFilter.TODO:
+      return list.filter(todo => !todo.isDone);
+
+    default:
+      return list;
+  }
+}
+
+export function pluralize(count, word) {
+  return count !== 1 ? `${word}s` : word;
+}
