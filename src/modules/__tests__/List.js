5a3b5ae3a68df7eff4246453067ea695f4564c51
Daniel André da Silva <dap1995@gmail.com>
2018-06-02 12:23:46 -0300

    Test filterListByVisibility function

--- src/modules/__tests__/List.js
+++ src/modules/__tests__/List.js
@@ -1,4 +1,8 @@
-import { pluralize } from '../List';
+import {
+  pluralize,
+  VisibilityFilter as Visibility,
+  filterListByVisibility
+} from '../List';
 
 describe('list module', () => {
   describe('pluralize', () => {
@@ -15,6 +19,44 @@ describe('list module', () => {
     it('should returns singular word when count is 2', () => {
       const singularWord = pluralize(2, 'item');
       expect(singularWord).toEqual('items');
+    });
+  });
+
+  describe('filterListByVisibility', () => {
+    const isDone = true;
+    const list = [
+      { isDone },
+      { isDone: !isDone },
+      { isDone },
+      { isDone },
+      { isDone: !isDone },
+      { isDone }
+    ];
+
+    it('should none visibility returns the same list', () => {
+      expect(list.length).toEqual(6);
+      const filteredList = filterListByVisibility(list, Visibility.NONE);
+      expect(filteredList).toBe(list);
+    });
+
+    it('should done visibility returns only isDone list items', () => {
+      expect(list.length).toEqual(6);
+      const filteredList = filterListByVisibility(list, Visibility.DONE);
+      expect(filteredList.length).toEqual(4);
+      filteredList.forEach(i => expect(i.isDone).toBeTruthy());
+    });
+
+    it('should todo visibility returns only uncompleted list items', () => {
+      expect(list.length).toEqual(6);
+      const filteredList = filterListByVisibility(list, Visibility.TODO);
+      expect(filteredList.length).toEqual(2);
+      filteredList.forEach(i => expect(i.isDone).toBeFalsy());
+    });
+
+    it('should wrong visibility returns the same list items', () => {
+      expect(list.length).toEqual(6);
+      const filteredList = filterListByVisibility(list, 'SOME WRONG TEXT');
+      expect(filteredList).toBe(list);
     });
   });
 });
