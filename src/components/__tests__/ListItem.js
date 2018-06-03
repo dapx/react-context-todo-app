cf9a2773e35728f7e810d14001de5351978474dd
Daniel André da Silva <dap1995@gmail.com>
2018-06-03 12:07:10 -0300

    Wraps ListItem tests with a describe function

--- src/components/__tests__/ListItem.js
+++ src/components/__tests__/ListItem.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import ListItem from '../ListItem';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ListItem onClick={() => {}} />, div);
-});
+describe('ListItem Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ListItem onClick={() => {}} />, div);
+  });
 
-it('should be the same as the snapshot', () => {
-  const tree = renderer.create(<ListItem />).toJSON();
-  expect(tree).toMatchSnapshot();
+  it('should be the same as the snapshot', () => {
+    const tree = renderer.create(<ListItem />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
