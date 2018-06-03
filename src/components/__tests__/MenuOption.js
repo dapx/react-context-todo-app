bf5232fc6dd6a30c084804ab2e1a94681de7df7a
Daniel André da Silva <dap1995@gmail.com>
2018-06-03 13:00:07 -0300

    Test MenuOption enabled states

--- src/components/__tests__/MenuOption.js
+++ src/components/__tests__/MenuOption.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Option from '../MenuOption';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Option onClick={() => {}} />, div);
+describe('MenuOption Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Option onClick={() => {}} />, div);
+  });
+
+  describe('should be', () => {
+    it('enabled', () => {
+      const tree = renderer.create(<Option enabled={true} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('disabled', () => {
+      const tree = renderer.create(<Option enabled={false} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
 });
