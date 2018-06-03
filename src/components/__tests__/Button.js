9feff425704c0c7b4ad313d00bb0fa09018ddea2
Daniel André da Silva <dap1995@gmail.com>
2018-06-02 23:07:39 -0300

    Test Button snapshot

--- src/components/__tests__/Button.js
+++ src/components/__tests__/Button.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Button from '../Button';
 
 it('renders without crashing', () => {
   const button = document.createElement('button');
   ReactDOM.render(<Button onClick={() => {}} />, button);
+});
+
+it('should be the same as the snapshot', () => {
+  const tree = renderer.create(<Button />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
