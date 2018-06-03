2af7e886dceb380e674f74de7a9ea717745cbce4
Daniel André da Silva <dap1995@gmail.com>
2018-06-02 23:18:37 -0300

    Test ListItem snapshot

--- src/components/__tests__/ListItem.js
+++ src/components/__tests__/ListItem.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import ListItem from '../ListItem';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ListItem onClick={() => {}} />, div);
+});
+
+it('should be the same as the snapshot', () => {
+  const tree = renderer.create(<ListItem />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
