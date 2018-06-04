60d3f1034bb4c5fc52bcb79b329838ca6e350d91
Daniel André da Silva <dap1995@gmail.com>
2018-06-04 19:24:14 -0300

    Move Todo module tests inside of a describe test function

--- src/modules/__tests__/Todo.js
+++ src/modules/__tests__/Todo.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
 import TodoProvider from '../Todo';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TodoProvider />, div);
-});
+describe('Todo Module', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<TodoProvider />, div);
+  });
 
-it('should create provider context state', () => {
-  const wrapper = mount(<TodoProvider />);
-  expect(wrapper.state('list')).toEqual([]);
+  it('should create provider context state', () => {
+    const wrapper = mount(<TodoProvider />);
+    expect(wrapper.state('list')).toEqual([]);
+  });
 });
