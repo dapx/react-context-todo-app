9fae489e616eec08918447dbbf566d991aa0c903
Daniel André da Silva <dap1995@gmail.com>
2018-06-04 19:22:01 -0300

    Test TodoProvider state creation

--- src/modules/__tests__/Todo.js
+++ src/modules/__tests__/Todo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import TodoProvider from '../Todo';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<TodoProvider />, div);
+});
+
+it('should create provider context state', () => {
+  const wrapper = mount(<TodoProvider />);
+  expect(wrapper.state('list')).toEqual([]);
 });
